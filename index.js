59d0996279c0603a3ea58af4dd72400d9c92f4e5
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2023-03-28 22:26:00 +0300

    hbs logic and Material UI

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express')
-const path = require('path')
+const exphbs = require('express-handlebars')
+
 const app = express()
 
-app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, 'views', 'index.html'))
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
 
-app.get('/about', (request, response) => {
-  response.sendFile(path.join(__dirname, 'views', 'about.html'))
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static('public'))
+
+app.get('/', (request, response) => {
+  response.render('index', {
+    title: 'Main page',
+    isHome: true
+  })
+})
+
+app.get('/add', (request, response) => {
+  response.render('add', {
+    title: 'Add course',
+    isAdd: true
+  })
+})
+
+app.get('/courses', (request, response) => {
+  response.render('courses', {
+    title: 'Courses',
+    isCourses: true
+  })
 })
 
 const PORT = process.env.PORT || 3000
---
+++ routes/home.js
@@ -0,0 +1,9 @@
+// const { Router } = require('express')
+// const router = Router()
+//
+// router.get('/', (request, response) => {
+//     response.render('index', {
+//         title: 'Main page',
+//         isHome: true
+//     })
+// })
