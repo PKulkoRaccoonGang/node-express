297892845fbed9d6ebb09e5d8e4e0f6723408539
PKulkoRaccoonGang <peter.kulko@raccoongang.com>
2023-03-28 12:59:49 +0300

    setup express and basic routing

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
-
+const path = require('path')
 const app = express()
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/about', (request, response) => {
+  response.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
 
 const PORT = process.env.PORT || 3000
 
